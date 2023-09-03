6f6348fa049b71b355e5a30ec17d4299c76d86b4
Faktorlande <kra1992@mail.ru>
2023-09-03 08:58:32 +0300

    solution repare for BigO

--- 888. Fair Candy Swap.py
+++ 888. Fair Candy Swap.py
@@ -4,21 +4,24 @@ class Solution:
     def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
         sum_b = sum(bobSizes)
         sum_a = sum(aliceSizes)
-        target =int((sum_a + sum_b)/2)
+        setB = set(bobSizes)
+        for x in aliceSizes:
+            if (x + (sum_b - sum_a)/2) in setB:
+                return [x, int(x + (sum_b - sum_a)/2)]
 
 
-        for i in range(len(aliceSizes)):
-            for j in range(len(bobSizes)):
-                if sum_a - aliceSizes[i] + bobSizes[j] == target:
 
-                    return list((aliceSizes[i], bobSizes[j]))
-
-
+        # for i in range(len(aliceSizes)):
+        #     for j in range(len(bobSizes)):
+        #         if sum_a - aliceSizes[i] + bobSizes[j] == target:
+        #
+        #             return list((aliceSizes[i], bobSizes[j])), serv_lst
 
 
 
 sol = Solution()
-aliceSizes = [2]
-bobSizes = [1,3]
+aliceSizes = [1, 4]
+bobSizes = [8, 5]
 
 print(sol.fairCandySwap(aliceSizes, bobSizes))
+#[1, 4, 0, 1]
